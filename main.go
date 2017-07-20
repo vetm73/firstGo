bb0b392d6fcd16c71772e689711cde6f01caddc1
Mike <mike@seventythree.cc>
2017-07-20 20:46:15 +0200

    fixes

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
-package firstGo
+package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func CityHandler(res http.ResponseWriter, req *http.Request) {
+	data, _ := json.Marshal("{'cities':'San Francisco, Amsterdam, Berlin, New York','Tokyo'}")
+	res.Header().Set("Content-Type", "application/json; charset=utf-8")
+	res.Write(data)
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+	http.HandleFunc("/cities.json", CityHandler)
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
